ba571ff089b5e612a61a288965f70d7c14117fe3
unknown <mbp818@gmail.com>
2022-10-31 18:12:23 -0700

    evaluating player guesses functionality

--- script.js
+++ script.js
@@ -30,14 +30,57 @@ let equationObject = {};
 const wrongFormat = [];
 
 // Time
+let timer;
+let timePlayed = 0;
+let baseTime = 0;
+let penaltyTime = 0;
+let finalTimeDisplay = '0.0s';
 
 // Scroll
 let valueY = 0;
 
 
+// Stop Timer, Process Results, go to score page
+function checkTime() {
+  console.log(timePlayed);
+  if(playerGuessArray.length == questionAmount) {
+    console.log('player guess array:', playerGuessArray);
+    clearInterval(timer);
+    // Check for wrong guesses, add penalty time
+    equationsArray.forEach((equation, index) => {
+       if (equation.evaluated === playerGuessArray[index]) {
+        // Corrct Guess, No penalty
+       } else {
+        // Incorrect Guess, add penalty
+        penaltyTime += 0.5;
+       }
+    });
+    finalTime = timePlayed + penaltyTime;
+    console.log('time', timePlayed, 'penalty', penaltyTime, 'final:', finalTime);
+  }
+}
+
+
+// Add tenth of a second to timePlayed
+function addTime() {
+  timePlayed += 0.1;
+  checkTime();
+}
+
+// Start timer when game page is clicked
+function startTimer() {
+  //reset times
+  timePlayed = 0;
+  penaltyTime = 0;
+  finalTime = 0;
+  timer = setInterval(addTime, 100);
+  gamePage.removeEventListener('click', startTimer);
+}
+
+
 // scroll, Store user selection in playerGuessArray
 function select(guessedTrue) {
-  console.log('player guess array:', playerGuessArray);
+  
   // Scroll 80px at a time
   valueY += 80;
   itemContainer.scroll(0, valueY);
@@ -190,3 +233,4 @@ startForm.addEventListener('click', () => {
 
 // Event Listeners 
 startForm.addEventListener('submit', selectQuestionAmount);
+gamePage.addEventListener('click', startTimer);
